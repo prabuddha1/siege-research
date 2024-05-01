ecc55999c3e2ba8000de16b004ba2958f167724b
Sophie Walden <sophie.walden@maine.edu>
2024-05-01 10:51:08 -0400

    updated styling

--- src/tabs/HomeTab.jsx
+++ src/tabs/HomeTab.jsx
@@ -38,26 +38,33 @@ function HomeTab() {
             <h2 className="research-area-title">Research Areas</h2>
 
             <div className="research-area-container">
-                <div className='research-area-display offset-color2'>
-                    <h3>Artificial Intelligence</h3>
-                    <img className="research-area-image" src="https://static.vecteezy.com/system/resources/previews/010/518/719/original/artificial-intelligence-ai-processor-chip-icon-symbol-for-graphic-design-logo-website-social-media-mobile-app-ui-illustration-vector.jpg"></img>
-                    <h3>Learn More!</h3>
-                </div>
-            
+                <a href="https://ieeexplore.ieee.org/abstract/document/9732465">                
+                  <div className='research-area-display offset-color2'>
+                      <h3>Artificial Intelligence</h3>
+                      <img className="research-area-image" src="https://cdn-icons-png.flaticon.com/512/826/826118.png"></img>
+                      <h3>Learn More!</h3>
+                  </div>
 
+                </a>
+
+            
+                <a href="https://ieeexplore.ieee.org/abstract/document/9732465">
                 <div className='research-area-display offset-color1'>
                     <h3>Internet-of-Things</h3>
                     <img className="research-area-image" src="https://cdn-icons-png.flaticon.com/512/2808/2808777.png"></img>
                     <h3>Learn More!</h3>
                 </div>
+                </a>
 
-
+                <a href="https://ieeexplore.ieee.org/abstract/document/8607163">
                 <div className='research-area-display offset-color2'>
                     <h3>System Security</h3>
                     <img className="research-area-image" src="https://cdn-icons-png.flaticon.com/512/4152/4152814.png"></img>
                     
                     <h3>Learn More!</h3>
                 </div>
+                </a>
+
             </div>
            
             
@@ -101,7 +108,37 @@ function HomeTab() {
         </div>
 
       </div>
+
+      <div className="recent-achievements">
+          <h2 className="section-title">Recent Achievements & Notable Contributions</h2>
+
+          <div className="achievements-container">
+            <h3>Selected Achievements</h3>
+            <ul>
+              <li>TTTC’s E. J. McCluskey Best Doctoral Thesis 2022 Award</li>
+              <li>Top Picks in Hardware and Embedded Security 2021, IEEE HSTTC</li>
+              <li>Innovation of the Year Award, University of Florida, 2022</li>
+              <li>Featured work on IEEE Spectrum for innovative electric vehicle charging technologies</li>
+            </ul>
+
+            <h3>Notable Research Contributions</h3>
+            <ul>
+              <li>HASTE: Software Security Analysis for Timing Attacks on Clear Hardware</li>
+              <li>SAIL: Analyzing Structural Artifacts of Logic Locking Using Machine Learning</li>
+              <li>MAGIC: Machine Learning Guided Image Compression for IoT</li>
+              <li>BINGO: Brain-Inspired Learning Memory Network</li>
+            </ul>
+          </div>
+      </div>
       
+      <div className="site-footer">
+          <h4>Contact Information</h4>
+          <p>Email: <a href="mailto:prabuddha@maine.edu">prabuddha@maine.edu</a></p>
+          <p>Explore More: <a href="https://www.google.com/scholar?q=Prabuddha+Chakraborty">Google Scholar Profile</a></p>
+          <p>Download: <a href="ProfileResumeLink">Resume</a> (updated 12/10/2023)</p>
+        </div>
+
+
     </div>
   );
 }
--- src/tabs/HumanTab.jsx
+++ src/tabs/HumanTab.jsx
@@ -34,28 +34,6 @@ function HumanTab() {
               <StaffCard key={index} name={item["name"]} title={item["title"]} email={item['email']} image={item['image']}></StaffCard>
             ))}
 
-            {/* <div className="staff-display-row">
-                <StaffCard name="Prabuddha Chakraborty" title="Lab Lead" email="prabuddha@maine.edu" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSXwkDpRFXNG8EG553wBPW7Xxoe--u3I3b-8zdy-Oc3A&s"></StaffCard>
-                <StaffCard name="Tanzim Mahfuz" title="Doctoral Student" email="tanzim.mahfuz@maine.edu" image="https://media.licdn.com/dms/image/C5603AQHWW9-44qQMAw/profile-displayphoto-shrink_400_400/0/1636832320122?e=1718236800&v=beta&t=4wRDiuHAu2V9c_ctNrleObEjOCaYt29a3YNTxYyLLAA"></StaffCard>
-                <StaffCard name="Tasneem Suha" title="Doctoral Student" email="tasneem.suha@maine.edu" image="https://media.licdn.com/dms/image/C5603AQHIYqj4_VzG9Q/profile-displayphoto-shrink_400_400/0/1659473672610?e=1718236800&v=beta&t=43nVPDYDWyzwaZv-yjWyYMr3ki9_LTJeJLlfMDW4_pQ"></StaffCard>
-            </div>
-
-            <div className="staff-display-row">
-                <StaffCard name="Md Hafizur Rahman" title="Doctoral Student" email="hafizur.rahman@maine.edu" image="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=sB5h9xUAAAAJ&citpid=8"></StaffCard>
-                <StaffCard name="Zafaryab Haider" title="Doctoral Student" email="zafaryab.haider@maine.edu" image="https://media.licdn.com/dms/image/D4E03AQETrHzQ61p62A/profile-displayphoto-shrink_400_400/0/1705363941368?e=1718236800&v=beta&t=MZTJfmaaUgk14NZvoBd4HEdmO0TW25xBB8bCp0Mj3Kw"></StaffCard>
-                <StaffCard name="Sarah Glatter" title="Masters Student" email="sarah.glatter@maine.edu" image="https://umaine.edu/compumaine/wp-content/uploads/sites/495/2021/03/Sarah-Glatter.jpeg"></StaffCard>
-   
-
-            </div>
-
-            <div className="staff-display-row">
-                <StaffCard name="Katherine Arsenault" title="Masters Student" email="katherine.arsenault@maine.edu" image="https://media.licdn.com/dms/image/D4E03AQGRJLok3gvcwA/profile-displayphoto-shrink_800_800/0/1687732429007?e=2147483647&v=beta&t=0urSTGgZNcKichLpkB6qhMw2IxCLoUAT76LDlSqO8mU"></StaffCard>
-                <StaffCard name="Nate Lowry" title="Undergraduate Student" email="nate.lowry@maine.edu" image="https://umaine.edu/vemi/wp-content/themes/umaine/content-blocks/people-list/silhouette.png"></StaffCard>
-                <StaffCard name="Collin Scobie" title="Undergraduate Studentd" email="collin.scobie@maine.edu" image="https://umaine.edu/hackerspace/wp-content/uploads/sites/290/2023/08/Collin.jpg"></StaffCard>
-   
-
-            </div> */}
-
         </div>
 
          </div>
